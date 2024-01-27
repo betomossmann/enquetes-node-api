d3a588057bf535a8698fffcd256e88b22d85bf6f
Gilberto Mossmann <gilbertomossmann@gmail.com>
2024-01-27 15:53:55 -0300

    test: ensure emailValidatorAdapter returns false if validator returns false

---
+++ tests/infra/email-validator-adapter.spec.ts
@@ -0,0 +1,11 @@
+import { EmailValidatorAdapter } from '@/infra/validators'
+
+import { describe, expect, it } from 'vitest'
+
+describe('EmailValidator Adapter', () => {
+  it('Should return false if validator returns false', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('invalid_email@mail.com')
+    expect(isValid).toBe(false)
+  })
+})
