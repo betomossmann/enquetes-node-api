d0e9682c716d759a743302523b0cbe1fb1b0c689
Gilberto Mossmann <gilbertomossmann@gmail.com>
2024-01-27 15:18:33 -0300

    chore: add config to vitest take config from tsconfig

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,9 @@
-import path from 'path'
 import { defineConfig } from 'vitest/config'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+  plugins: [tsconfigPaths()],
   test: {
-    environment: 'happy-dom',
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
+    environment: 'happy-dom'
   }
 })
