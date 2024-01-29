38a75066f879070a5ec217a0499b9281a3c9aff0
Gilberto Mossmann <gilbertomossmann@gmail.com>
2024-01-28 21:29:13 -0300

    chore: add vitest configs for mongodb

---
+++ tests/global.d.ts
@@ -0,0 +1,2 @@
+// eslint-disable-next-line @typescript-eslint/naming-convention
+declare let __MONGO_URI__: string
--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
+    setupFiles: ['./setup/mongo-memory-server.ts'],
     coverage: {
       all: false,
       include: ['src'],
