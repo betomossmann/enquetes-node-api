94966941a87082bb8ca55ce279a3680fbc1eceee
Gilberto Mossmann <gilbertomossmann@gmail.com>
2024-01-28 21:16:14 -0300

    refactor: change configuration vitest for coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,5 +2,12 @@ import { defineConfig } from 'vitest/config'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [tsconfigPaths()]
+  plugins: [tsconfigPaths()],
+  test: {
+    coverage: {
+      all: false,
+      include: ['src'],
+      exclude: ['src/**/*-test.ts', 'src/**/*-spec.ts']
+    }
+  }
 })
