4839cece4ac03c4a44e7bc9b69f00b90c17a3784
Gilberto Mossmann <gilbertomossmann@gmail.com>
2024-01-29 14:44:35 -0300

    test: ensure requests parse body as json

---
+++ tests/main/body-parser.test.ts
@@ -0,0 +1,12 @@
+import app from '@/main/config/app'
+
+import Request from 'supertest'
+
+describe('Body Parser Middleware', () => {
+  it('Should parse body as json', () => {
+    app.post('/test_body_parser', (req, res) => {
+      res.send(req.body)
+    })
+    return Request(app).post('/test_body_parser').send({ name: 'any_name' }).expect({ name: 'any_name' })
+  })
+})
