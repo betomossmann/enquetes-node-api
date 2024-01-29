dd03bb06361c3cc1ddb40b4ca0b06be92fb550ad
Gilberto Mossmann <gilbertomossmann@gmail.com>
2024-01-29 14:59:44 -0300

    test: ensure returns Content-Type default as json

---
+++ tests/main/content-type.test.ts
@@ -0,0 +1,13 @@
+import app from '@/main/config/app'
+
+import request from 'supertest'
+
+describe('Content Type Middleware', () => {
+  test('Should return default content type as json', async () => {
+    app.get('/test_content_type', (req, res) => res.send(''))
+    await request(app)
+      .get('/test_content_type')
+      // If there is a JSON word in any type of content it will be accepted by default
+      .expect('content-type', /json/)
+  })
+})
